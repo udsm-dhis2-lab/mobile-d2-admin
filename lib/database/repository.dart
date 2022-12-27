3bcc0f01dd49d509b40fb17a4663860ece97f465
Yuseif <yuseifabdillah@gmail.com>
2022-12-27 18:31:44 +0300

    fea: Add database interface
    All databases types will have to implement this interface

---
+++ lib/database/repository.dart
@@ -0,0 +1,17 @@
+import '../models/models.dart';
+abstract class Repository {
+  Stream<List<Instance>> watchAllInstances();
+
+  Future<int> addInstance(Instance instance);
+
+  Future updateInstance(Instance instance);
+
+  Future removeInstance(Instance instance);
+
+  Stream<List<InstancesPingStatus>> searchInstancesPingStatusByInstanceId(
+      Instance instance);
+
+  Future removeInstancePingStatus(InstancesPingStatus status);
+
+  Future<int> addInstancePingStatus(InstancesPingStatus status);
+}
