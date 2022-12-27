c6071311483b969c2183f09a09470a2c63912011
Yuseif <yuseifabdillah@gmail.com>
2022-12-27 18:02:38 +0300

    feat: Add Instances model

---
+++ lib/models/instances.dart
@@ -0,0 +1,11 @@
+class Instances {
+  int id;
+  String instanceName;
+  String instanceUrl;
+
+  Instances({
+    required this.id,
+    required this.instanceName,
+    required this.instanceUrl
+  });
+}
