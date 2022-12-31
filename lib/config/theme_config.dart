6cdaa13ac31e9e39dfbe6b5d7d5ea3d7fd0b0991
Yuseif <yuseifabdillah@gmail.com>
2022-12-31 15:24:47 +0300

    feat: Add constant app colors

---
+++ lib/config/theme_config.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color.fromRGBO(71, 181, 255, 1);
+  static const onPrimaryColor = Color(0xFFFFFFFF);
+  static const primaryContainerColor = Color.fromRGBO(71, 181, 255, 0.4);
+  static const onPrimaryContainerColor = Color(0xFFFFFFFF);
+  static const backgroundColor = Color(0xFFFFFFFF);
+  static const surfaceColor = Color(0xFFFFFFFF);
+  static const onSurfaceColor = Color(0xFF000000);
+  static const successColor = Color(0xFF3CCF4E);
+  static const errorColor = Color(0xFFFF1E00);
+  static const textMuted = Color(0xFFAD9D9D);
+}
