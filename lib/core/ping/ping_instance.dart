54c4cf9b7bdf5a3fece6c104a1d4a19e0d0b25c4
Yuseif <yuseifabdillah@gmail.com>
2023-01-01 19:02:46 +0300

    fix: Add check to see if instances are available before pinging

--- lib/core/ping/ping_instance.dart
+++ lib/core/ping/ping_instance.dart
@@ -8,11 +8,12 @@ class PingInstance {
 
   PingInstance({required this.repository});
 
-  pingInstances() async {
+  void pingInstances() async {
     // get all instances
     List<Instance> instances = await repository.getAllInstances();
 
-    for (var instance in instances) {
+    if (instances.isNotEmpty){
+      for (var instance in instances) {
       // ping an instance
       var url = Uri.parse(instance.instanceUrl);
       var req = http.Request('GET', url);
@@ -25,7 +26,8 @@ class PingInstance {
           statusCode: res.statusCode.toString(),
           pingTime: DateTime.now());
       // add it to the database
-      return repository.addInstancePingStatus(status);
+    repository.addInstancePingStatus(status);
+    }
     }
   }
 }
