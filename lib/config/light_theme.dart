e2907209e1c0a38be326486298d50b5122ad2f96
Yuseif <yuseifabdillah@gmail.com>
2023-01-09 01:51:40 +0300

    feat: Add new font sizes and weights

--- lib/config/light_theme.dart
+++ lib/config/light_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class D2AdminLightTheme {
   static TextTheme lightTextTheme = const TextTheme(
@@ -7,7 +8,7 @@ class D2AdminLightTheme {
       fontWeight: FontWeight.w700,
     ),
     titleLarge: TextStyle(
-      fontSize: 16,
+      fontSize: 18,
       fontWeight: FontWeight.w600,
     ),
     titleMedium: TextStyle(
@@ -16,11 +17,11 @@ class D2AdminLightTheme {
     ),
     titleSmall: TextStyle(fontSize: 10, fontWeight: FontWeight.normal),
     bodySmall: TextStyle(
-      fontSize: 8,
-      fontWeight: FontWeight.normal,
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
     ),
     labelMedium: TextStyle(
-      fontSize: 16,
+      fontSize: 20,
       fontWeight: FontWeight.bold,
     ),
     labelSmall: TextStyle(
@@ -33,4 +34,10 @@ class D2AdminLightTheme {
     brightness: Brightness.light,
     textTheme: lightTextTheme,
   );
+
+// make the whole app theme use roboto google font
+  static ThemeData buildTheme() {
+    return lightTheme.copyWith(
+        textTheme: GoogleFonts.robotoTextTheme(lightTheme.textTheme));
+  }
 }
