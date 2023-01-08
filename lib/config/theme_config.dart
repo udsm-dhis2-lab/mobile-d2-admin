2e1614c590a8d0bce13934c789980e67fd86e904
Yuseif <yuseifabdillah@gmail.com>
2023-01-09 01:52:26 +0300

    Add new color values

--- lib/config/theme_config.dart
+++ lib/config/theme_config.dart
@@ -3,13 +3,19 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const primaryColor = Color(0xFF448BFF);
   static const onPrimaryColor = Color(0xFFFFFFFF);
-  static const primaryContainerColor = Color.fromRGBO(255, 255, 255, 0.3);
+  static const primaryContainerColor = Color(0xFF659FFF);
   static const onPrimaryContainerColor = Color(0xFFFFFFFF);
+  static const secondaryColor = Color(0xFFE8F0FE);
+  static const onSecondaryColor = Color(0xFF6180E8);
+  static const secondaryContainer = Color(0xFFE8F0FE);
+  static const onSecondaryContainerColor = Color(0xFF6180E8);
   static const backgroundColor = Color(0xFFFFFFFF);
   static const surfaceColor = Color(0xFFFFFFFF);
   static const onSurfaceColor = Color(0xFF000000);
-  static const successColor = Color(0xFF3CCF4E);
-  static const errorColor = Color(0xFFFF1E00);
+  static const successColor = Color(0xFF38CF62);
+  static const errorColor = Color(0xFFFF7B73);
   static const outlineColor = Color(0xFFE5E5E5);
   static const textMuted = Color(0xFFAD9D9D);
+  static const progressIndicatorColor = Color(0xFFA9CAFF);
+  static const appBlackColor = Color(0xFF202124);
 }
