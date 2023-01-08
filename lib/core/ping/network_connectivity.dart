ce5e2c6b5b45e6375e26b1777b4de0f29311bb1f
Yuseif <yuseifabdillah@gmail.com>
2023-01-08 22:36:42 +0300

    Rename class to  NetworkConnectivty

---
+++ lib/core/ping/network_connectivity.dart
@@ -0,0 +1,17 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class NetworkConnectivity {
+  static final Connectivity _connectivity = Connectivity();
+
+  static Future<bool> checkIfConnected() async {
+    final ConnectivityResult connectivityResult =
+        await _connectivity.checkConnectivity();
+
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      return true;
+    }
+
+    return false;
+  }
+}
